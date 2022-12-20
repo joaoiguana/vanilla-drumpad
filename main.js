1baf3f3955e7dd46d5788a6e98b87338700f79a9
joao garcia <jiboiadamor@gmail.com>
2022-12-20 20:22:32 +0000

    added the rest of the 9 sounds and some minimal css styling

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const kick = document.getElementById('kick');
 const snare = document.getElementById('snare');
 const chord = document.getElementById('chord');
+const hatClosed = document.getElementById('hat-closed');
+const hatOpen = document.getElementById('hat-open');
+const crash = document.getElementById('crash');
+const tomMid = document.getElementById('tom-mid');
+const tomHigh = document.getElementById('tom-high');
+const bass = document.getElementById('bass');
 
 document.addEventListener('keydown', (e) => {
   if (e.key === "v") {
@@ -13,6 +19,24 @@ document.addEventListener('keydown', (e) => {
     chord.loop = false;
     chord.currentTime = 0;
     chord.play();
+  } else if (e.key === "i") {
+    hatClosed.currentTime = 0;
+    hatClosed.play();
+  } else if (e.key === "k") {
+    hatOpen.currentTime = 0;
+    hatOpen.play();
+  } else if (e.key === "e") {
+    crash.currentTime = 0;
+    crash.play();
+  } else if (e.key === "g") {
+    tomMid.currentTime = 0;
+    tomMid.play();
+  } else if (e.key === "h") {
+    tomHigh.currentTime = 0;
+    tomHigh.play();
+  } else if (e.key === "b") {
+    bass.currentTime = 0;
+    bass.play();
   }
 });
 
